282d5cd1869fb8f9234629c56718373518fa0a17
weinee <weinee24@gmail.com>
2022-05-05 17:43:28 +0800

    Connecting to db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
+
+connectDB();
 
 const app = express();
 

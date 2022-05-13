a17aaaf102209b8ed2ec7453f03c3d3328a8082d
weinee <weinee24@gmail.com>
2022-05-13 16:51:45 +0800

    Remove redundant functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -36,16 +36,14 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found.");
   }
 
-  const user = await User.findById(req.user.id);
-
   //Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
   //Make sure user owns goal
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("Not authorized");
   }
@@ -68,16 +66,14 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found.");
   }
 
-  const user = await User.findById(req.user.id);
-
   //Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
   //Make sure user owns goal
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("Not authorized");
   }

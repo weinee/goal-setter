7c16fd644cefb0b16d842b64a96bc31a1c711a20
weinee <weinee24@gmail.com>
2022-05-13 12:29:24 +0800

    Implement cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const connectDB = require("./config/db");
@@ -9,6 +10,18 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 connectDB();
 
 const app = express();
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
